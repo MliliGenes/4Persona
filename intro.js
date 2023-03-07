70212b3d1f6aeadc258340c619773f42c9c962e7
saad/saax <120732259+saaxbutcool@users.noreply.github.com>
2023-03-07 17:05:49 +0100

    Delete intro.js

--- intro.js
+++
@@ -1,7 +0,0 @@
-const intro = document.querySelector(".intro");
-const h1 = document.querySelector(".into-title");
-window.addEventListener("load", (e) => {
-  setTimeout((e) => {
-    intro.classList.add("action");
-  }, 1400);
-});
