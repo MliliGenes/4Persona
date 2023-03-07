d6a264f7037a948582289901ffa1246c4f8969d8
saad/saax <120732259+saaxbutcool@users.noreply.github.com>
2023-03-07 02:42:09 +0100

    Add files via upload

--- cursor.js
+++ cursor.js
@@ -1,7 +1,5 @@
 const cursor = document.querySelector(".cursor");
-console.log(cursor);
 window.addEventListener("mousemove", (e) => {
-  console.log("saad");
   cursor.setAttribute(
     "style",
     "top : " + (e.pageY - 15) + "px; left: " + (e.pageX - 15) + "px"
