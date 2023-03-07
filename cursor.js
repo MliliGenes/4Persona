c7239845d35f2e3b3a44dd5a2cca740655690af2
saad/saax <120732259+saaxbutcool@users.noreply.github.com>
2023-03-07 22:52:47 +0100

    Add files via upload

--- cursor.js
+++ cursor.js
@@ -2,6 +2,6 @@ const cursor = document.querySelector(".cursor");
 window.addEventListener("mousemove", (e) => {
   cursor.setAttribute(
     "style",
-    "top : " + (e.pageY - 15) + "px; left: " + (e.pageX - 15) + "px"
+    "top : " + (e.clientY - 15) + "px; left: " + (e.clientX - 15) + "px"
   );
 });
--- intro.js
+++ intro.js
@@ -1,7 +1,12 @@
 const intro = document.querySelector(".intro");
 const h1 = document.querySelector(".into-title");
+
 window.addEventListener("load", (e) => {
   setTimeout((e) => {
     intro.classList.add("action");
-  }, 1400);
+  }, 4000);
+});
+
+window.addEventListener("click", (e) => {
+  intro.classList.add("action");
 });
